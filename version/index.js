73769acca90003f14777f53472560bc17849ad49
bluefoundry <bluefoundry@noreply.com>
2025-10-16 15:41:30 +0900

    finalized

--- version/index.js
+++ version/index.js
@@ -125,7 +125,6 @@ async function action() {
       }
 
       let afterIndex = verIndex + version.length;
-      console.log(`after index ${afterIndex} and length ${clFirstLine.length}`)
       if (afterIndex < clFirstLine.length) {
         let after = clFirstLine.charCodeAt(afterIndex);
         if (after >= 48 && after <= 57) {
